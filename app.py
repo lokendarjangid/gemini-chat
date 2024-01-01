c210d8dd61d7b379f49a355475a89a53d525c7d7
lokendar jangid <lokendarjangid1234@gmail.com>
2024-01-01 22:08:05 +0530

    new commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import geminiSrc
 
 app = Flask(__name__)
+app.secret_key = "433e732fe46eb93bed8f0392"
+app.permanent_session_lifetime = 360
+
 
 # initialization of gemini
 gemini = geminiSrc.Gemini()
@@ -10,23 +12,27 @@ gemini = geminiSrc.Gemini()
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    session.permanent = True
+    session_data = session.get('output_dict', {})
     if request.method == 'POST':
         task_content = request.form['ask']
 
         try:
-            file = open('output.txt', 'w')
             output = gemini.generate(task_content)
-            file.write(output)
-            file.close()
+            session_data[task_content] = output
+            session['output_dict'] = session_data
             return redirect('/')
         except:
             return 'There was an issue connecting to Gemini.'
 
     else:
         # tasks = Todo.query.order_by(Todo.date_created).all()
-        file = open('output.txt', 'r')
-        file_output = file.read()
-        return render_template('index.html', output=file_output)
+        try :
+            session_data = session.get('output_dict', {})
+            print(session_data)
+            return render_template('index.html', output=session_data)
+        except:
+            return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- geminiSrc/__init__.py
+++ geminiSrc/__init__.py
@@ -16,27 +16,28 @@ class Gemini:
             "max_output_tokens": 2048,
         }
 
-        # safety_settings = [
-        #     {
-        #         "category": "HARM_CATEGORY_HARASSMENT",
-        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        #     },
-        #     {
-        #         "category": "HARM_CATEGORY_HATE_SPEECH",
-        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        #     },
-        #     {
-        #         "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        #     },
-        #     {
-        #         "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        #         "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        #     },
-        # ]
+        safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+            },
+        ]
 
         self.model = genai.GenerativeModel(model_name="gemini-pro",
-                                           generation_config=generation_config,)
+                                           generation_config=generation_config,
+                                           safety_settings=safety_settings)
 
     def generate(self, prompt):
         prompt_parts = [prompt]
